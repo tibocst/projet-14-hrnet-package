966015b022b64c8462fb66fbae4508652b6f273c
tibocst <thibault.coste.233@gmail.com>
2023-11-16 02:01:23 +0100

    ajout doc code

--- project-14-hrnet-modale/src/components/Modale.js
+++ project-14-hrnet-modale/src/components/Modale.js
@@ -3,14 +3,28 @@ import "./styles.css";
 import { useState } from "react";
 
 export function Modale(props) {
-    const {src} = props
-  const [close, setClose] = useState(false)
+  //React Component taking svg file to display a modale with custom close button
+  const { src } = props;
+  const [close, setClose] = useState(false);
 
-  return (<div className={close ? "modale_closed" : "modale"} onClick={() => setClose(true)}>
-    <div className="modale-opacity"></div>
-    <div className="modale-textbox" onClick={(e) => e.stopPropagation()}>
-      <p>Employee Created!</p>
-      <img src={src} className="modale-textbox_close" alt="close cross" onClick={() => setClose(true)}/>
+  return (
+    // it basicaly display a div on all page on top of other elements
+    <div
+      className={close ? "modale_closed" : "modale"}
+      onClick={() => setClose(true)}
+    >
+      {/* modale-opacity is used to display a grey color with low opacity */}
+      <div className="modale-opacity"></div>
+      <div className="modale-textbox" onClick={(e) => e.stopPropagation()}>
+        <p>Employee Created!</p>
+        {/* custom close button */}
+        <img
+          src={src}
+          className="modale-textbox_close"
+          alt="close cross"
+          onClick={() => setClose(true)}
+        />
+      </div>
     </div>
-  </div>);
+  );
 }
--- project-14-hrnet-modale/src/index.js
+++ project-14-hrnet-modale/src/index.js
@@ -6,7 +6,6 @@ import { Modale } from './components/Modale';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='test'>sdfgsdfsssssssssssssssssssssssssssssssssssssdfsfsfk hdfvskn ln fbdshj vdhbjvsdhbvsdhbvsdbhvdsbh</div>
     <Modale />
   </React.StrictMode>
 );
